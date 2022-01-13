789f321c9e925296f6a28cb1575bc006a6798374
HaykAvagyan1 <hayk.avagyan@tumo.org>
2022-01-13 19:02:17 +0400

    path correction for server

--- scripts/network.js
+++ scripts/network.js
@@ -3,11 +3,11 @@ const network = {
         let url = new URL(document.location.href);
         let lang = url.searchParams.get("lang");
         let name = url.searchParams.get("name");
-        let getString = `../data/${lang}/${name}.json`;
+        let getString = `data/${lang}/${name}.json`;
         let data = {};
 
         if (lang === null || name == null) {
-            await $.get(`../data/FR/first.json`, function (json) { data = json; });
+            await $.get(`data/FR/first.json`, function (json) { data = json; });
         } else {
             await $.get(getString, function (json) { data = json; });
         }
